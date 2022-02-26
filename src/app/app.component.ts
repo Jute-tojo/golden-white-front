1c0d0cd5f8f625b7ab7f75d5211a1c6298eed39a
jute <tojoramaroson5@gmail.com>
2022-02-26 22:59:15 +0800

    premier commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin':'*'
+  })
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+  title = 'GOLDEN WHITE';
+
+  produits: any;
+  uri: string = "https://golden-white.herokuapp.com/api/";
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get(this.uri)
+        .subscribe(
+            data => {
+              this.produits = data;
+            }
+        );
+  }
+
+  ngOnInit(): void {
+
+  }
 }
